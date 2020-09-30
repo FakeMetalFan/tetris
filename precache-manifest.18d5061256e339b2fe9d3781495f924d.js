caff66a5327c5f3d0ab58a5336e53513a7d69a36
FakeMetalFan <fakemetalfan@gmail.com>
2020-09-30 19:31:42 +0300

    Updates

---
+++ precache-manifest.18d5061256e339b2fe9d3781495f924d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee891cc9b287bb31bc990404b6abb5ea",
+    "url": "/tetris/index.html"
+  },
+  {
+    "revision": "d494eacaa5c57553901a",
+    "url": "/tetris/static/css/main.a54178d8.chunk.css"
+  },
+  {
+    "revision": "b67adccc738ffcf9382a",
+    "url": "/tetris/static/js/2.8c838398.chunk.js"
+  },
+  {
+    "revision": "bfd04eb851fbc8ea43c375cc9d698376",
+    "url": "/tetris/static/js/2.8c838398.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d494eacaa5c57553901a",
+    "url": "/tetris/static/js/main.4bc44884.chunk.js"
+  },
+  {
+    "revision": "c179afea1aa9726cb16b",
+    "url": "/tetris/static/js/runtime-main.0d8747ab.js"
+  }
+]);
