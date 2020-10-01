dc1232cc48e6a96ed1630c3f8586a4a0818a1dff
FakeMetalFan <fakemetalfan@gmail.com>
2020-10-01 16:36:49 +0300

    Feature: removed redundant stuff;

--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -1,7 +1,34 @@
 import React, { memo } from 'react';
 
-import { getTileClassName } from 'utils/tile-class-name';
+import { tileFill } from 'const';
 
 import './Tile.scss';
 
-export const Tile = memo(({ state }) => (<div className={getTileClassName(state.fill)} />));
+export const Tile = memo(({ state: { fill } }) => {
+  const getTileClassName = () => {
+    const prefix = 'tile tile__';
+
+    switch (fill) {
+      case tileFill.None:
+        return `${prefix}none`;
+      case tileFill.I:
+        return `${prefix}i`;
+      case tileFill.J:
+        return `${prefix}j`;
+      case tileFill.L:
+        return `${prefix}l`;
+      case tileFill.O:
+        return `${prefix}o`;
+      case tileFill.S:
+        return `${prefix}s`;
+      case tileFill.T:
+        return `${prefix}t`;
+      case tileFill.Z:
+        return `${prefix}z`;
+      default:
+        throw new Error(`Unexpected tile fill of "${fill}"!`);
+    }
+  };
+
+  return (<div className={getTileClassName()} />);
+});
