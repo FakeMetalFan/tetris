1178f279961a2cadf7b9f85201be50db39cead74
FakeMetalFan <fakemetalfan@gmail.com>
2020-10-01 16:49:31 +0300

    Feature: refactored Tile className computation;

--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -5,30 +5,36 @@ import { tileFill } from 'const';
 import './Tile.scss';
 
 export const Tile = memo(({ state: { fill } }) => {
-  const getTileClassName = () => {
-    const prefix = 'tile tile__';
+  let className = 'tile tile__';
 
-    switch (fill) {
-      case tileFill.None:
-        return `${prefix}none`;
-      case tileFill.I:
-        return `${prefix}i`;
-      case tileFill.J:
-        return `${prefix}j`;
-      case tileFill.L:
-        return `${prefix}l`;
-      case tileFill.O:
-        return `${prefix}o`;
-      case tileFill.S:
-        return `${prefix}s`;
-      case tileFill.T:
-        return `${prefix}t`;
-      case tileFill.Z:
-        return `${prefix}z`;
-      default:
-        throw new Error(`Unexpected tile fill of "${fill}"!`);
-    }
-  };
+  switch (fill) {
+    case tileFill.None:
+      className += 'none';
+      break;
+    case tileFill.I:
+      className += 'i';
+      break;
+    case tileFill.J:
+      className += 'j'
+      break;
+    case tileFill.L:
+      className += 'l';
+      break;
+    case tileFill.O:
+      className += 'o';
+      break;
+    case tileFill.S:
+      className += 's';
+      break;
+    case tileFill.T:
+      className += 't';
+      break;
+    case tileFill.Z:
+      className += 'z';
+      break;
+    default:
+      throw new Error(`Unexpected tile fill of "${fill}"!`);
+  }
 
-  return (<div className={getTileClassName()} />);
+  return (<div className={className} />);
 });
