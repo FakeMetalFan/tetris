f1ac7e199563a0d3a55e81a4b54c20c8c77f2a1c
FakeMetalFan <fakemetalfan@gmail.com>
2020-10-01 11:55:05 +0300

    Feature: simplified tetrominos storing / retrieving;

--- src/hooks/use-tetromino.js
+++ src/hooks/use-tetromino.js
@@ -1,11 +1,10 @@
 import { useState } from 'react';
 
-import { tetrominoMap } from 'const';
+import { tetrominos } from 'const';
 
 export const useTetromino = ({ width }) => {
   const getRandomTetromino = () => {
-    const keys = [...tetrominoMap.keys()];
-    const item = tetrominoMap.get(keys[Math.random() * keys.length | 0]);
+    const item = tetrominos[Math.random() * tetrominos.length | 0];
 
     return item.getCloned((width - item.width) / 2 | 0);
   };
