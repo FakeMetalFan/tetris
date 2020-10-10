415ad7cc5bdce6ba5115f0a5acb30ed0425f6dfd
FakeMetalFan <fakemetalfan@gmail.com>
2020-10-10 10:53:10 +0300

    Feature: code style;

--- src/App.js
+++ src/App.js
@@ -2,10 +2,8 @@ import React from 'react';
 
 import { Header, Tetris, Footer } from 'components';
 
-export const App = () => (
-  <>
-    <Header />
-    <Tetris />
-    <Footer />
-  </>
-);
+export const App = () => <>
+  <Header />
+  <Tetris />
+  <Footer />
+</>;
