938b3bb4e968f6c3d19b5da1af1e42fa4ce2127b
FakeMetalFan <fakemetalfan@gmail.com>
2021-06-13 00:18:48 +0300

    Tech: unit tests: removed redundant "expect"-s

--- src/__tests__/hooks/useTetris.test.ts
+++ src/__tests__/hooks/useTetris.test.ts
@@ -30,12 +30,6 @@ describe('useTetris', () => {
     (tetrominos as any).default = [oTetromino];
 
     const { result } = renderHook(() => useTetris(params));
-    const { tiles } = result.current;
-
-    expect(tiles[0][1].fill).toBe(TileFill.O);
-    expect(tiles[0][2].fill).toBe(TileFill.O);
-    expect(tiles[1][1].fill).toBe(TileFill.O);
-    expect(tiles[1][2].fill).toBe(TileFill.O);
 
     (tetrominos as any).default = [zTetromino];
 
@@ -45,24 +39,18 @@ describe('useTetris', () => {
       });
     }
 
-    const { tiles: nextTiles } = result.current;
+    const { tiles } = result.current;
 
-    expect(nextTiles[0][0].fill).toBe(TileFill.Z);
-    expect(nextTiles[0][1].fill).toBe(TileFill.Z);
-    expect(nextTiles[1][1].fill).toBe(TileFill.Z);
-    expect(nextTiles[1][2].fill).toBe(TileFill.Z);
+    expect(tiles[0][0].fill).toBe(TileFill.Z);
+    expect(tiles[0][1].fill).toBe(TileFill.Z);
+    expect(tiles[1][1].fill).toBe(TileFill.Z);
+    expect(tiles[1][2].fill).toBe(TileFill.Z);
   });
 
   it('should auto drop tetromino', () => {
     (tetrominos as any).default = [oTetromino];
 
     const { result } = renderHook(() => useTetris(params));
-    const { tiles } = result.current;
-
-    expect(tiles[0][1].fill).toBe(TileFill.O);
-    expect(tiles[0][2].fill).toBe(TileFill.O);
-    expect(tiles[1][1].fill).toBe(TileFill.O);
-    expect(tiles[1][2].fill).toBe(TileFill.O);
 
     const autoDropTimesCount = 2;
 
@@ -70,24 +58,18 @@ describe('useTetris', () => {
       jest.advanceTimersByTime(autoDropMs * autoDropTimesCount);
     });
 
-    const { tiles: nextTiles } = result.current;
+    const { tiles } = result.current;
 
-    expect(nextTiles[autoDropTimesCount][1].fill).toBe(TileFill.O);
-    expect(nextTiles[autoDropTimesCount][2].fill).toBe(TileFill.O);
-    expect(nextTiles[autoDropTimesCount + 1][1].fill).toBe(TileFill.O);
-    expect(nextTiles[autoDropTimesCount + 1][2].fill).toBe(TileFill.O);
+    expect(tiles[autoDropTimesCount][1].fill).toBe(TileFill.O);
+    expect(tiles[autoDropTimesCount][2].fill).toBe(TileFill.O);
+    expect(tiles[autoDropTimesCount + 1][1].fill).toBe(TileFill.O);
+    expect(tiles[autoDropTimesCount + 1][2].fill).toBe(TileFill.O);
   });
 
   it('should enable / disable auto drop', () => {
     (tetrominos as any).default = [oTetromino];
 
     const { result } = renderHook(() => useTetris(params));
-    const { tiles: initialTiles } = result.current;
-
-    expect(initialTiles[0][1].fill).toBe(TileFill.O);
-    expect(initialTiles[0][2].fill).toBe(TileFill.O);
-    expect(initialTiles[1][1].fill).toBe(TileFill.O);
-    expect(initialTiles[1][2].fill).toBe(TileFill.O);
 
     act(() => {
       result.current.setIsAutoDrop(false);
@@ -122,18 +104,6 @@ describe('useTetris', () => {
     (tetrominos as any).default = [oTetromino];
 
     const { result } = renderHook(() => useTetris(params));
-    const { tiles: initialTiles } = result.current;
-
-    expect(initialTiles[0][1].fill).toBe(TileFill.O);
-    expect(initialTiles[0][2].fill).toBe(TileFill.O);
-    expect(initialTiles[1][1].fill).toBe(TileFill.O);
-    expect(initialTiles[1][2].fill).toBe(TileFill.O);
-    expect(initialTiles[2][1].fill).toBe(TileFill.Empty);
-    expect(initialTiles[2][2].fill).toBe(TileFill.Empty);
-    expect(initialTiles[3][1].fill).toBe(TileFill.Empty);
-    expect(initialTiles[3][2].fill).toBe(TileFill.Empty);
-    expect(initialTiles[4][1].fill).toBe(TileFill.Empty);
-    expect(initialTiles[4][2].fill).toBe(TileFill.Empty);
 
     act(() => {
       result.current.setMove(createMove(MoveCode.Left));
@@ -181,12 +151,6 @@ describe('useTetris', () => {
     (tetrominos as any).default = [oTetromino];
 
     const { result } = renderHook(() => useTetris(params));
-    const { tiles } = result.current;
-
-    expect(tiles[0][1].fill).toBe(TileFill.O);
-    expect(tiles[0][2].fill).toBe(TileFill.O);
-    expect(tiles[1][1].fill).toBe(TileFill.O);
-    expect(tiles[1][2].fill).toBe(TileFill.O);
 
     for (let i = 0; i < 5; i += 1) {
       act(() => {
@@ -194,24 +158,18 @@ describe('useTetris', () => {
       });
     }
 
-    const { tiles: nextTiles } = result.current;
+    const { tiles } = result.current;
 
-    expect(nextTiles[0][0].fill).toBe(TileFill.O);
-    expect(nextTiles[0][1].fill).toBe(TileFill.O);
-    expect(nextTiles[1][0].fill).toBe(TileFill.O);
-    expect(nextTiles[1][1].fill).toBe(TileFill.O);
+    expect(tiles[0][0].fill).toBe(TileFill.O);
+    expect(tiles[0][1].fill).toBe(TileFill.O);
+    expect(tiles[1][0].fill).toBe(TileFill.O);
+    expect(tiles[1][1].fill).toBe(TileFill.O);
   });
 
   it('should move tetromino to the right without colliding', () => {
     (tetrominos as any).default = [oTetromino];
 
     const { result } = renderHook(() => useTetris(params));
-    const { tiles } = result.current;
-
-    expect(tiles[0][1].fill).toBe(TileFill.O);
-    expect(tiles[0][2].fill).toBe(TileFill.O);
-    expect(tiles[1][1].fill).toBe(TileFill.O);
-    expect(tiles[1][2].fill).toBe(TileFill.O);
 
     for (let i = 0; i < 5; i += 1) {
       act(() => {
@@ -219,24 +177,18 @@ describe('useTetris', () => {
       });
     }
 
-    const { tiles: nextTiles } = result.current;
+    const { tiles } = result.current;
 
-    expect(nextTiles[0][2].fill).toBe(TileFill.O);
-    expect(nextTiles[0][3].fill).toBe(TileFill.O);
-    expect(nextTiles[1][2].fill).toBe(TileFill.O);
-    expect(nextTiles[1][3].fill).toBe(TileFill.O);
+    expect(tiles[0][2].fill).toBe(TileFill.O);
+    expect(tiles[0][3].fill).toBe(TileFill.O);
+    expect(tiles[1][2].fill).toBe(TileFill.O);
+    expect(tiles[1][3].fill).toBe(TileFill.O);
   });
 
   it('should rotate tetromino without colliding', () => {
     (tetrominos as any).default = [zTetromino];
 
     const { result } = renderHook(() => useTetris(params));
-    const { tiles } = result.current;
-
-    expect(tiles[0][0].fill).toBe(TileFill.Z);
-    expect(tiles[0][1].fill).toBe(TileFill.Z);
-    expect(tiles[1][1].fill).toBe(TileFill.Z);
-    expect(tiles[1][2].fill).toBe(TileFill.Z);
 
     act(() => {
       result.current.setMove(createMove(MoveCode.Left));
@@ -256,25 +208,18 @@ describe('useTetris', () => {
       });
     }
 
-    const { tiles: nextTiles } = result.current;
+    const { tiles } = result.current;
 
-    expect(nextTiles[0][1].fill).toBe(TileFill.Z);
-    expect(nextTiles[1][0].fill).toBe(TileFill.Z);
-    expect(nextTiles[1][1].fill).toBe(TileFill.Z);
-    expect(nextTiles[2][0].fill).toBe(TileFill.Z);
+    expect(tiles[0][1].fill).toBe(TileFill.Z);
+    expect(tiles[1][0].fill).toBe(TileFill.Z);
+    expect(tiles[1][1].fill).toBe(TileFill.Z);
+    expect(tiles[2][0].fill).toBe(TileFill.Z);
   });
 
   it('should clear filled row and set score', () => {
     (tetrominos as any).default = [oTetromino];
 
     const { result } = renderHook(() => useTetris(params));
-    const { tiles, score } = result.current;
-
-    expect(tiles[0][1].fill).toBe(TileFill.O);
-    expect(tiles[0][2].fill).toBe(TileFill.O);
-    expect(tiles[1][1].fill).toBe(TileFill.O);
-    expect(tiles[1][2].fill).toBe(TileFill.O);
-    expect(score).toBe(0);
 
     act(() => {
       result.current.setMove(createMove(MoveCode.Left));
@@ -286,16 +231,16 @@ describe('useTetris', () => {
       jest.advanceTimersByTime(autoDropMs * 4);
     });
 
-    const { tiles: nextTiles, score: nextScore } = result.current;
+    const { tiles, score } = result.current;
 
-    expect(nextTiles[3][0].fill).toBe(TileFill.Empty);
-    expect(nextTiles[3][1].fill).toBe(TileFill.Empty);
-    expect(nextTiles[3][2].fill).toBe(TileFill.Empty);
-    expect(nextTiles[3][3].fill).toBe(TileFill.Empty);
-    expect(nextTiles[4][0].fill).toBe(TileFill.Empty);
-    expect(nextTiles[4][1].fill).toBe(TileFill.Empty);
-    expect(nextTiles[4][2].fill).toBe(TileFill.Empty);
-    expect(nextTiles[4][3].fill).toBe(TileFill.Empty);
-    expect(nextScore).toBe(20);
+    expect(tiles[3][0].fill).toBe(TileFill.Empty);
+    expect(tiles[3][1].fill).toBe(TileFill.Empty);
+    expect(tiles[3][2].fill).toBe(TileFill.Empty);
+    expect(tiles[3][3].fill).toBe(TileFill.Empty);
+    expect(tiles[4][0].fill).toBe(TileFill.Empty);
+    expect(tiles[4][1].fill).toBe(TileFill.Empty);
+    expect(tiles[4][2].fill).toBe(TileFill.Empty);
+    expect(tiles[4][3].fill).toBe(TileFill.Empty);
+    expect(score).toBe(20);
   });
 });
