74911b7ae976e6ec7e50a01ca198f30a68ff27af
FakeMetalFan <fakemetalfan@gmail.com>
2021-06-22 17:06:35 +0300

    Tech: removed redundant App component;

--- src/App.tsx
+++
@@ -1,5 +0,0 @@
-import Tetris from 'containers/Tetris';
-
-const App = () => <Tetris width={10} height={20} />;
-
-export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import 'index.scss';
 
-import App from 'App';
+import Tetris from 'containers/Tetris';
 import { StrictMode } from 'react';
 import { render } from 'react-dom';
 import setupIcons from 'setupIcons';
@@ -9,7 +9,7 @@ setupIcons();
 
 render(
   <StrictMode>
-    <App />
+    <Tetris width={10} height={20} />
   </StrictMode>,
   document.getElementById('root')
 );
