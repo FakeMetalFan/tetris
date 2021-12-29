251aa12e226cbb59126a2aa2fbe45f3ea830c592
FakeMetalFan <fakemetalfan@gmail.com>
2021-12-29 09:04:34 +0200

    code style;

--- src/utils/compose.ts
+++ src/utils/compose.ts
@@ -1,6 +1,9 @@
 export default (...fns: ((...args: any[]) => any)[]) =>
   (...args: any[]) => {
-    const [first, ...rest] = fns;
+    const [
+      first,
+      ...rest
+    ] = fns;
 
     return rest.reduce((acc, fn) => fn(acc), first(...args));
   };
